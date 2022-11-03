739f34f95fa112f1cc12866ee06108ef71ef451a
Sushriya <sushriya1009@gmail.com>
2022-11-03 20:58:00 +0530

    added files

--- server.py
+++ server.py
@@ -1,12 +1,24 @@
 
-from flask import Flask
+from unicodedata import name
+from flask import Flask , redirect,url_for
 
 app = Flask(__name__)
 
 @app.route('/admin')
-def hw(name):
-    return "juyiiiiiiiiiiii  %s"%name
-
+def admin():
+    return "occhev entra admin uu"
+@app.route('/guest/<name>')
+def guest(name):
+    if name!="admin":
+        return "kottha ga occhav entra %s"% name
+    
+    return redirect(url_for('admin'))
+@app.route('/user/<name>')
+def check(name):
+    if name=='admin':
+        return redirect (url_for('admin'))
+    else:
+        return redirect(url_for('guest',name=name))
 if __name__=="__main__":
     app.debug=True
     app.run(port=5000)
